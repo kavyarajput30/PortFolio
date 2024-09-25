fa1d5c873ff5db0480d8f6550c3cb181b264f5e1
kavya rajput <rajputkavya2004@gmail.com>
2024-09-25 09:48:40 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,31 +17,21 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
-              <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
-              <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
-              </p>
+            <h3>Full Stack Developer</h3>
+<p>
+  Specializing in Creating Seamless, Responsive, and Optimized Web Solutions
+</p>
+
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
-              <p>
-                I have designed multiple landing pages and have created design
-                systems as well
-              </p>
+<h3>App Developer</h3>
+<p>
+  Building High-Performance, Cross-Platform Mobile Apps with React Native Expertise
+</p>
+
             </div>
           </li>
         </ul>
